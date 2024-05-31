bca1c097b253bc052ee3ead61c2dc7217ee267e6
zshahab91 <zshahab91@gmail.com>
2024-05-31 06:23:15 +0200

    Update employee.service.ts

--- services/employee.service.ts
+++ services/employee.service.ts
@@ -6,17 +6,3 @@ export const getAllEmployees = async (): Promise<IEmployee[]> =>{
     const data = await fetch(`${url}/employees`);
     return await data.json() ?? [];
 }
-
-// async saveAllMachinesData(data: IMachine[]): Promise<IMachine[]> {
-//     let result: IMachine[] = [];
-//     try {
-//         const response = await fetch(`${this.url}/save`, {
-//             method: "POST",
-//             body: JSON.stringify(data)
-//         });
-//         result = await response.json();
-//     } catch (error) {}
-//     return result;
-// }
-
-
